6cc367b2a71ddefc078a069a9b2d118885577d7a
ambekadeshmukh <ambeka@live.com>
2024-09-05 23:08:15 -0400

    script edited

--- cloud-resume-project/script.js
+++ cloud-resume-project/script.js
@@ -1,7 +1,13 @@
-async function getVisitorCount() {
-    const response = await fetch('https://l87arummtg.execute-api.us-east-1.amazonaws.com/prod');
-    const data = await response.json();
-    document.getElementById('visitor-count').innerText = data.count;
-  }
-  
-  getVisitorCount();
+
+  async function getVisitorCount() {
+    try {
+        const response = await fetch('https://l87arummtg.execute-api.us-east-1.amazonaws.com/prod');
+        const data = await response.json();
+        document.getElementById('visitor-count').innerText = data.count;
+    } catch (error) {
+        console.error('Error fetching visitor count:', error);
+        document.getElementById('visitor-count').innerText = 'Error loading count';
+    }
+}
+
+getVisitorCount();
